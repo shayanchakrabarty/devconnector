b8ec70f1dce1b58b41e0e257c5ee215a3fa85ad5
shayan chakraborty <shayan.chakrabarty@rediffmail.com>
2019-04-17 05:25:28 +0530

    create profile commit

---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,125 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import axios from 'axios';
+//import classname from 'classnames';
+import { connect } from 'react-redux';
+import { registerUser } from '../../actions/authActions';
+import TextFieldGroup from '../common/TextFieldGroup';
+
+class Register extends Component {
+
+	onSubmit(e) {
+		e.preventDefault();
+
+		const newUser = {
+			name: this.state.name,
+			email: this.state.email,
+			password: this.state.password,
+			password2: this.state.password2
+		};
+
+		//console.log(newUser);
+		// axios
+		// 	.post('/api/users/register', newUser)
+		// 	.then(user => {console.log(user.data);})
+		// 	//.catch(err => console.log(err.response.data));
+		// 	.catch(err => this.setState({ errors: err.response.data }));
+		this.props.registerUser(newUser, this.props.history);
+	}
+
+	componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+
+	componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+	onChange(e) {
+		this.setState({ [e.target.name]: e.target.value });
+	}
+	
+	constructor() {
+		super();
+
+		this.state = {
+			name: '',
+			email: '',
+			password: '',
+			password2: '',
+			errors: {}
+		};
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	render() { 
+		const { errors } = this.state; //object destrucring
+		// const { user } = this.props.auth;
+		return ( 
+		<div className="register">
+			{/* { user ? user.name : null } */}
+			<div className="container">
+				<div className="row">
+					<div className="col-md-8 m-auto">
+						<h1 className="display-4 text-center">Sign Up</h1>
+						<p className="lead text-center">Create your DevConnector account</p>
+						<form noValidate onSubmit={this.onSubmit}>
+						<TextFieldGroup
+                  placeholder="Name"
+                  name="name"
+                  value={this.state.name}
+                  onChange={this.onChange}
+                  error={errors.name}
+                />
+                <TextFieldGroup
+                  placeholder="Email"
+                  name="email"
+                  type="email"
+                  value={this.state.email}
+                  onChange={this.onChange}
+                  error={errors.email}
+                  info="This site uses Gravatar so if you want a profile image, use a Gravatar email"
+                />
+                <TextFieldGroup
+                  placeholder="Password"
+                  name="password"
+                  type="password"
+                  value={this.state.password}
+                  onChange={this.onChange}
+                  error={errors.password}
+                />
+                <TextFieldGroup
+                  placeholder="Confirm Password"
+                  name="password2"
+                  type="password"
+                  value={this.state.password2}
+                  onChange={this.onChange}
+                  error={errors.password2}
+                />
+							<input type="submit" className="btn btn-info btn-block mt-4" />
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		 );
+	}
+}
+
+Register.propTypes = {
+	registerUser: PropTypes.func.isRequired,
+	auth: PropTypes.object.isRequired,
+	errors: PropTypes.object.isRequired
+};
+const mapStateToProps = state => ({
+	auth: state.auth,
+	errors: state.errors
+});
+ 
+export default connect(mapStateToProps, {registerUser})(Register);
