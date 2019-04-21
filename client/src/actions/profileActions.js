5d682f02ce94b92644520d2a84ab4e3a474c27ff
shayan chakraborty <shayan.chakrabarty@rediffmail.com>
2019-04-22 00:17:48 +0530

    display profile commit

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
+import { GET_PROFILE,  GET_PROFILES, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS, SET_CURRENT_USER } from './types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -21,6 +21,45 @@ export const getCurrentProfile = () => dispatch => {
 		);
 };
 
+
+// Get profile by handle
+export const getProfileByHandle = handle => dispatch => {
+  dispatch(setProfileLoading());
+  axios
+    .get(`/api/profile/handle/${handle}`)
+    .then(res =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })
+    )
+    .catch(err =>
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+};
+
+
+//Get All Profiles
+export const getProfiles = () => dispatch => {
+	dispatch(setProfileLoading());
+	axios
+		.get('/api/profile/all')
+		.then(res => 
+			dispatch({
+				type: GET_PROFILES,
+				payload: res.data
+			})
+		)
+		.catch(err => 
+			dispatch({
+				type: GET_PROFILES,
+				payload: null
+			})	
+		)
+};
 
 // Delete Experience
 export const deleteExperience = id => dispatch => {
